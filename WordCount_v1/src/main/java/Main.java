d10c4a3fbbdaa07d447f823acaa262743409ca6c
Kushal <kushal@crayondata.com>
2015-06-26 13:58:04 +0530

    fist map-reduce test program

---
+++ WordCount_v1/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Main {
+
+	public static void main(String[] args) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException{
+		
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf,"Word Count");
+		job.setJarByClass(Main.class);
+		job.setMapperClass(MapWords.class);
+		job.setCombinerClass(ReduceWords.class);
+		job.setReducerClass(ReduceWords.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job,new Path(""));
+		FileOutputFormat.setOutputPath(job, new Path(""));
+		System.exit(job.waitForCompletion(true)?0:1);
+		
+	}
+	
+}
