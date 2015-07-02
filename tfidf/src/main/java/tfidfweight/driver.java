e85a5dfe2b46c3d11927df9e8d7134e480df204a
Kushal <kushal@crayondata.com>
2015-07-02 15:36:36 +0530

    Bugs Fixed related

--- tfidf/src/main/java/tfidfweight/driver.java
+++ tfidf/src/main/java/tfidfweight/driver.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -32,7 +33,7 @@ public class driver {
 		job.setMapOutputValueClass(Text.class);
 		
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(FloatWritable.class);
 		
 		FileOutputFormat.setOutputPath(job,new Path(opPath) );
 		
--- tfidf/src/main/java/tfidfweight/redd.java
+++ tfidf/src/main/java/tfidfweight/redd.java
@@ -4,35 +4,49 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class redd extends Reducer<Text,Text,Text,Text>{
+public class redd extends Reducer<Text,Text,Text,FloatWritable>{
 
-	private float idf;
+	private float idf=0;
 	
 	@Override
 	public void reduce(Text key,Iterable<Text> values,Context context) throws IOException, InterruptedException{
 		
-		List<Text> tempVal=new ArrayList<Text>();
+//		List<Text> tempVal=new ArrayList<Text>();
+
+		List<String> temp= new ArrayList<String>();
 		
-		for(Text val:values){		
-			if(val.toString().indexOf(' ')==-1)
-				{idf=Float.parseFloat(val.toString());}
-			else
-				{tempVal.add(val);}
-		}
+		int sum=0;
+		for(Text val:values){
+
+			String s=val.toString();
+			sum++;
+			if(s.indexOf(" ")==-1)
+				{idf=Float.parseFloat(s);}
+			else{temp.add(s);}
+			
+		}		
+
+		//context.write(new Text("FUCK"),new FloatWritable((float)(sum)));
+//			else
+//				{tempVal.add(val);}
+//		}
 		//context.write(key, new Text(Float.toString(idf)));
 		
-//		for(Text val:values)			
+		for(String val:temp)
+			context.write(new Text(val.substring(0,val.indexOf(' '))+" "+key.toString()),new FloatWritable((Float.parseFloat(val.substring(val.indexOf(' ')+1))*idf)));
+
+//		
+//		for(Text val:tempVal){
 //			if(val.toString().indexOf(' ')!=-1)
 //				context.write(new Text(val.toString().substring(0,val.toString().indexOf(' '))+" "+key.toString()+" "),new Text(Double.toString(Double.parseDouble(val.toString().substring(val.toString().indexOf(' ')+1))*idf)));
+//		}
 
-		
-		for(Text val:tempVal){
-			if(val.toString().indexOf(' ')!=-1)
-				context.write(new Text(val.toString().substring(0,val.toString().indexOf(' '))+" "+key.toString()+" "),new Text(Double.toString(Double.parseDouble(val.toString().substring(val.toString().indexOf(' ')+1))*idf)));
-		}
+//		for(Text val:values)
+//			context.write(key,val);
 		
 	}
 	
